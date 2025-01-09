822de7ce5e55fc1ee36c77d5759b52a28b61a27d
SANKET7837 <sanketpawar30612@gmail.com>
2025-01-09 17:21:49 +0530

    upadate the button type

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,29 @@
-import React from 'react';
-import { Header } from './ui/components/header';
-import {Page1 } from './ui/components/page1';
-import {Page2 } from './ui/components/page2';
-import {Page3 } from './ui/components/page3';
-import {Page4 } from './ui/components/page4';
-import {Page5 } from './ui/components/page5';
-import {Page6 } from './ui/components/page6';
-import {Page7 } from './ui/components/page7';
+import React from "react";
+import { Header } from "./ui/components/header";
+import { Page1 } from "./ui/components/page1";
+import { Page2 } from "./ui/components/page2";
+import { Page3 } from "./ui/components/page3";
+import { Page4 } from "./ui/components/page4";
+import { Page5 } from "./ui/components/page5";
+import { Page6 } from "./ui/components/page6";
+import { Page7 } from "./ui/components/page7";
 
 const App = () => {
-  return (
-   <>
-    <Header/> 
-<Page1/>
-<Page2/>
-  <Page3/>
-  <Page4/>
+	return (
+		<>
+			<Header />
+			<Page1 />
+			<Page2 />
+			<Page3 />
+			<Page4 />
 
- 
-<Page5/>
+			<Page5 />
 
+			<Page6 />
 
-   <Page6/>
-
-   
-  <Page7/>
-
-
-     
-
-    </>
-    
-  );
+			<Page7 />
+		</>
+	);
 };
 
 export default App;
--- src/ui/components/page4.jsx
+++ src/ui/components/page4.jsx
@@ -1,123 +1,123 @@
-import React from 'react'
+import React from "react";
 
 export const Page4 = () => {
-  return (
-    <div className="p-4 md:p-44 bg-gray-100 text-left">
-    <h1 className="text-xl md:text-6xl text-black  font-bold m- p-12 flex justify-center">My Experiences</h1>
-    <p className="text-sm md:text-2xl m-4 text-pink-600 text-center  ">One of the most beautiful parts of every person's life, the part of their work life, I am very happy to have spent this part of my life with the people I loved and it has made my interest in my work more and more.</p>
+	return (
+		<div className="p-4 md:p-44 bg-gray-100 text-left">
+			<h1 className="text-xl md:text-6xl text-black  font-bold m- p-12 flex justify-center">
+				My Experiences
+			</h1>
+			<p className="text-sm md:text-2xl m-4 text-pink-600 text-center  ">
+				One of the most beautiful parts of every person's life, the part of
+				their work life, I am very happy to have spent this part of my life with
+				the people I loved and it has made my interest in my work more and more.
+			</p>
 
+			<div class="flex flex-wrap justify-center gap-6 p-6 bg-gray-100">
+				<div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
+					<div class="flex items-center mb-4">
+						<div class="w-10 h-10 flex items-center justify-center bg-green-200 rounded-full">
+							<img src="public/image2.png" alt="Wallet Icon" class="w-6 h-6" />
+						</div>
+						<h2 class="text-lg font-semibold ml-4">
+							Full-stack Developer - FinlyWealth
+							<span class="text-sm text-gray-500">(Full Time)</span>
+						</h2>
+					</div>
 
-<div class="flex flex-wrap justify-center gap-6 p-6 bg-gray-100">
-  
-  <div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
-    
-    <div class="flex items-center mb-4">
-      <div class="w-10 h-10 flex items-center justify-center bg-green-200 rounded-full">
-       
-        <img src="public/image2.png" alt="Wallet Icon" class="w-6 h-6" />
-      </div>
-      <h2 class="text-lg font-semibold ml-4">
-        Full-stack Developer - FinlyWealth 
-        <span class="text-sm text-gray-500">(Full Time)</span>
-      </h2>
-    </div>
-    
-    <div class="text-orange-600 font-medium text-sm mb-4">
-      2024 - Now
-    </div>
-   
-    <ul class="list-disc pl-6 text-gray-700">
-      <li>Chrome extension development focused on user-friendly features.</li>
-      <li>Developed front-end and back-end for finance and purchasing projects.</li>
-      <li>Improved workflows for better efficiency and data security.</li>
-      <li>Analyzed performance and resolved issues for system.</li>
-    </ul>
-  </div>
+					<div class="text-orange-600 font-medium text-sm mb-4">2024 - Now</div>
 
-  
-  <div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
-   
-    <div class="flex items-center mb-4">
-      <div class="w-10 h-10 flex items-center justify-center bg-blue-200 rounded-full">
-        
-        <img src="public/image3.png" alt="Hand Icon" class="w-6 h-6" />
-      </div>
-      <h2 class="text-lg font-semibold ml-4">
-        Full Stack Developer - Dastyar 
-        <span class="text-sm text-orange-600">(Part time - Freelancer)</span>
-      </h2>
-    </div>
- 
-    <div class="text-red-700 font-medium text-sm mb-4">
-      2023 - 2024
-    </div>
-    
-    <ul class="list-disc pl-6 text-gray-700">
-      <li>Extension development for different browsers with 200,000 users.</li>
-      <li>Designed and developed server-side programs to communicate with extensions.</li>
-      <li>Scaled the project to support millions of users.</li>
-      <li>Troubleshot application and code issues.</li>
-    </ul>
-  </div>
-</div>
+					<ul class="list-disc pl-6 text-gray-700">
+						<li>
+							Chrome extension development focused on user-friendly features.
+						</li>
+						<li>
+							Developed front-end and back-end for finance and purchasing
+							projects.
+						</li>
+						<li>Improved workflows for better efficiency and data security.</li>
+						<li>Analyzed performance and resolved issues for system.</li>
+					</ul>
+				</div>
 
-<div class="flex flex-wrap justify-center gap-6 p-6 bg-gray-100">
-  
-  <div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
-    
-    <div class="flex items-center mb-4">
-      <div class="w-10 h-10 flex items-center justify-center bg-green-200 rounded-full">
-       
-        <img src="public/image4.png" alt="Wallet Icon" class="w-6 h-6" />
-      </div>
-      <h2 class="text-lg font-semibold ml-4">
-        Full-stack Developer - 7 Learn 
-        <span class="text-sm text-orange-500">(Full Time)</span>
-      </h2>
-    </div>
-    
-    <div class="text-orange-600 font-medium text-sm mb-4">
-      2024 - Now
-    </div>
-   
-    <ul class="list-disc pl-6 text-gray-700">
-      <li>Development of projects based on Sass architecture
-Optimizing the databases of the largest educational website in Iran
-Reviewing and refactoring projects and preparing for large scales
-Full stack development in different sections with Vue.js, Laravel, PHP stacks
+				<div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
+					<div class="flex items-center mb-4">
+						<div class="w-10 h-10 flex items-center justify-center bg-blue-200 rounded-full">
+							<img src="public/image3.png" alt="Hand Icon" class="w-6 h-6" />
+						</div>
+						<h2 class="text-lg font-semibold ml-4">
+							Full Stack Developer - Dastyar
+							<span class="text-sm text-orange-600">
+								(Part time - Freelancer)
+							</span>
+						</h2>
+					</div>
 
-</li>
-    </ul>
-  </div>
+					<div class="text-red-700 font-medium text-sm mb-4">2023 - 2024</div>
 
-  
-  <div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
-   
-    <div class="flex items-center mb-4">
-      <div class="w-10 h-10 flex items-center justify-center bg-blue-200 rounded-full">
-        
-        <img src="public/image5.png" alt="Hand Icon" class="w-6 h-6" />
-      </div>
-      <h2 class="text-lg font-semibold ml-4">
-        Full Stack Developer - Cowsel Software Solutions 
-        <span class="text-sm text-orange-600">(full time)</span>
-      </h2>
-    </div>
- 
-    <div class="text-red-700 font-medium text-sm mb-4">
-      2023 - 2024
-    </div>
-    
-    <ul class="list-disc pl-6 text-gray-700">
-      <li>Extension development for different browsers with 200,000 users.</li>
-      <li>Designed and developed server-side programs to communicate with extensions.</li>
-      <li>Scaled the project to support millions of users.</li>
-      <li>Troubleshot application and code issues.</li>
-    </ul>
-  </div>
-</div>
+					<ul class="list-disc pl-6 text-gray-700">
+						<li>
+							Extension development for different browsers with 200,000 users.
+						</li>
+						<li>
+							Designed and developed server-side programs to communicate with
+							extensions.
+						</li>
+						<li>Scaled the project to support millions of users.</li>
+						<li>Troubleshot application and code issues.</li>
+					</ul>
+				</div>
+			</div>
 
-  </div>
-  )
-}
+			<div class="flex flex-wrap justify-center gap-6 p-6 bg-gray-100">
+				<div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
+					<div class="flex items-center mb-4">
+						<div class="w-10 h-10 flex items-center justify-center bg-green-200 rounded-full">
+							<img src="public/image4.png" alt="Wallet Icon" class="w-6 h-6" />
+						</div>
+						<h2 class="text-lg font-semibold ml-4">
+							Full-stack Developer - 7 Learn
+							<span class="text-sm text-orange-500">(Full Time)</span>
+						</h2>
+					</div>
 
+					<div class="text-orange-600 font-medium text-sm mb-4">2024 - Now</div>
+
+					<ul class="list-disc pl-6 text-gray-700">
+						<li>
+							Development of projects based on Sass architecture Optimizing the
+							databases of the largest educational website in Iran Reviewing and
+							refactoring projects and preparing for large scales Full stack
+							development in different sections with Vue.js, Laravel, PHP stacks
+						</li>
+					</ul>
+				</div>
+
+				<div class="bg-white shadow-lg rounded-lg p-6 w-full sm:w-96">
+					<div class="flex items-center mb-4">
+						<div class="w-10 h-10 flex items-center justify-center bg-blue-200 rounded-full">
+							<img src="public/image5.png" alt="Hand Icon" class="w-6 h-6" />
+						</div>
+						<h2 class="text-lg font-semibold ml-4">
+							Full Stack Developer - Cowsel Software Solutions
+							<span class="text-sm text-orange-600">(full time)</span>
+						</h2>
+					</div>
+
+					<div class="text-red-700 font-medium text-sm mb-4">2023 - 2024</div>
+
+					<ul class="list-disc pl-6 text-gray-700">
+						<li>
+							Extension development for different browsers with 200,000 users.
+						</li>
+						<li>
+							Designed and developed server-side programs to communicate with
+							extensions.
+						</li>
+						<li>Scaled the project to support millions of users.</li>
+						<li>Troubleshot application and code issues.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
+};
